9dc1325d7cbe25ef734cd72c7dd573c65173980f
kongfl888 <kongfl888@outlook.com>
2023-03-19 12:23:19 +0800

    xbrz:adapt our project.
    Signed-off-by: kongfl888 <kongfl888@outlook.com>

--- xbrzfilter.h
+++ xbrzfilter.h
@@ -10,15 +10,15 @@
 
 void xbrz2x32(euI32* srcPtr, euI32 srcPitch, euI32* dstPtr, euI32 dstPitch, int width, int height)
 {
-    xbrz::scale(2, (const uint32_t*)srcPtr, (uint32_t*)dstPtr, width, height, xbrz::ColorFormat::RGB, srcPitch, dstPitch);
+	xbrz::scale(2, (const uint32_t*)srcPtr, (uint32_t*)dstPtr, width, height, xbrz::ColorFormat::RGB);
 }
 
 void xbrz3x32(euI32* srcPtr, euI32 srcPitch, euI32* dstPtr, euI32 dstPitch, int width, int height)
 {
-    xbrz::scale(3, (const uint32_t*)srcPtr, (uint32_t*)dstPtr, width, height, xbrz::ColorFormat::RGB, srcPitch, dstPitch);
+	xbrz::scale(3, (const uint32_t*)srcPtr, (uint32_t*)dstPtr, width, height, xbrz::ColorFormat::RGB);
 }
 
 void xbrz4x32(euI32* srcPtr, euI32 srcPitch, euI32* dstPtr, euI32 dstPitch, int width, int height)
 {
-    xbrz::scale(4, (const uint32_t*)srcPtr, (uint32_t*)dstPtr, width, height, xbrz::ColorFormat::RGB, srcPitch, dstPitch);
+	xbrz::scale(4, (const uint32_t*)srcPtr, (uint32_t*)dstPtr, width, height, xbrz::ColorFormat::RGB);
 }
